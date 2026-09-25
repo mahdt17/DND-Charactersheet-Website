00490a3041ae52940cb715cb5542dae4b80bf1a9
Mahdi17 <mahdi.benjamaa1017@gmail.com>
2026-09-25 02:55:05 -0400

    Load refined character sheet presentation

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {PresentationProvider} from "./PresentationSettings";
 import "./index.css";
 import "./modern.css";
 import "./fantasy.css";
+import "./refined.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
